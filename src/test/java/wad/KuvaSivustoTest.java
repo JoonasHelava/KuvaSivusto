513df5b4f584f8ffaeccf79c5910cc409da66ab1
JoonasHelava <43232859+JoonasHelava@users.noreply.github.com>
2018-09-26 12:09:08 +0300

    Add files via upload

--- src/test/java/wad/KuvaSivustoTest.java
+++ src/test/java/wad/KuvaSivustoTest.java
@@ -1,10 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package wad;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,8 +10,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -23,6 +22,8 @@ import org.springframework.web.context.WebApplicationContext;
  *
  * @author Joonas
  */
+
+//Test to check the basic functions of site
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class KuvaSivustoTest {
@@ -50,10 +51,35 @@ public class KuvaSivustoTest {
     }
     @Test
     public void canPostPicture() throws Exception {
-        // "aarrggghh" likely wont render -- we don't care :)
         MockMultipartFile multipartFile = new MockMultipartFile("file", "kuha.png", "image/png", "kuva".getBytes());
-
         mockMvc.perform(multipart(API_URI_SELAA).file(multipartFile))
                 .andExpect(redirectedUrl(API_URI_SELAA));
+    }
+    @Test
+    public void canUpvote() throws Exception {
+        this.postPicture();
+        mockMvc.perform(get(API_URI_AANESTA_PLUS))
+                .andExpect(redirectedUrl(API_URI_SELAA));
+    }
+    @Test
+    public void canDownvote() throws Exception {
+        this.postPicture();
+        mockMvc.perform(get(API_URI_AANESTA_MINUS))
+                .andExpect(redirectedUrl(API_URI_SELAA));
+    }
+    @Test
+    public void canPostCommentsAndContainsIt() throws Exception {
+        this.postPicture();
+        mockMvc.perform(
+                post(API_URI_SINGLE_KOMMENTOI).param("title", "Test").param("content", "Text")
+        ).andExpect(redirectedUrl(API_URI_SELAA));
+        MvcResult res = mockMvc.perform(get(API_URI_SINGLE_SELAA))
+                .andReturn();
+        String content = res.getResponse().getContentAsString();
+        Assert.assertTrue(content.contains("Test"));
+    }
+    public void postPicture() throws Exception{
+        MockMultipartFile multipartFile = new MockMultipartFile("file", "kuha.png", "image/png", "kuva".getBytes());
+        mockMvc.perform(multipart(API_URI_SELAA).file(multipartFile)).andReturn();
     }
 }
